150ec0450b6492d0c763ae53f3f0b2e737094406
Vishwajeet-Kr <bishwjit8890@gmail.com>
2026-02-22 00:46:56 +0530

    Added remove button

--- script.js
+++ script.js
@@ -22,50 +22,33 @@ function updateCart() {
   cartList.innerHTML = "";
   total = 0;
 
+//   for (let item in cart) {
+//     const li = document.createElement("li");
+
+//     const itemTotal = cart[item].price * cart[item].quantity;
+//     total += itemTotal;
+
+//     li.textContent = `${item} x ${cart[item].quantity} = ₹${itemTotal}`;
+//     cartList.appendChild(li);
+//   }
+
   for (let item in cart) {
-    const li = document.createElement("li");
+  const li = document.createElement("li");
 
-    const itemTotal = cart[item].price * cart[item].quantity;
-    total += itemTotal;
+  const itemTotal = cart[item].price * cart[item].quantity;
+  total += itemTotal;
 
-    li.textContent = `${item} x ${cart[item].quantity} = ₹${itemTotal}`;
-    cartList.appendChild(li);
-  }
+  li.innerHTML = `
+    ${item} x ${cart[item].quantity} = ₹${itemTotal}
+    <button onclick="decreaseItem('${item}')">➖</button>
+    <button onclick="removeItem('${item}')">❌</button>
+  `;
+
+  cartList.appendChild(li);
+}
 
   totalDisplay.textContent = total;
 }
-
-// function placeOrder() {
-
-//   const name = document.getElementById("customer-name").value;
-//   const table = document.getElementById("table-number").value;
-
-//   if (!name || !table) {
-//     alert("Please enter your name and table number");
-//     return;
-//   }
-
-//   if (Object.keys(cart).length === 0) {
-//     alert("Your cart is empty");
-//     return;
-//   }
-
-//   let itemsSummary = "";
-
-//   for (let item in cart) {
-//     itemsSummary += `${item} x ${cart[item].quantity}, `;
-//   }
-
-//   alert(
-//     `Order Placed!\n\nName: ${name}\nTable: ${table}\nItems: ${itemsSummary}\nTotal: ₹${total}`
-//   );
-
-//   // Reset cart after order
-//   cart = {};
-//   updateCart();
-//   document.getElementById("customer-name").value = "";
-//   document.getElementById("table-number").value = "";
-// }
 
 async function placeOrder() {
 
@@ -122,4 +105,18 @@ async function placeOrder() {
     console.error("Error:", error);
     alert("Failed to place order.");
   }
+}
+
+function decreaseItem(itemName) {
+  if (cart[itemName].quantity > 1) {
+    cart[itemName].quantity -= 1;
+  } else {
+    delete cart[itemName];
+  }
+  updateCart();
+}
+
+function removeItem(itemName) {
+  delete cart[itemName];
+  updateCart();
 }
